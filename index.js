7639316309a60aae75ada46e0eb6017a92e995a4
Ryan Michalowski <michalowskiryan3@gmail.com>
2026-06-11 15:37:32 -0700

    MongoDBSetup(Error Econnrefused)

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 require("dotenv").config()
-const mongoose =  require("mongoose")
+const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
-//const path = require("path")
+const path = require("path")
+
 app.use(express.json())
 
-const userRoute = require("./server/routes/user")
-
-mongoose.connect(process.env.ubURL)
+mongoose.connect(process.env.dbURL)
     .then(() => console.log("DB Connected!!"))
     .catch(error => console.log(error));
 
-//CORS middleware
+const userRoute = require("./server/routes/user")
+
+// CORS middleware
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -19,8 +20,8 @@ app.use(function(req, res, next) {
     next();
 });
 
-//app.use(express.static(__dirname + "/public"))
-//app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public", "index.html")))
+app.use(express.static(__dirname + "/public"))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public", "index.html")))
 
 app.use("/users", userRoute)
 
