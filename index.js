1faa931ed26c642d69d35e84b6ec5bb08321817e
Ryan Michalowski <michalowskiryan3@gmail.com>
2026-06-11 13:26:36 -0700

    MongoDB Setup

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import "dotenv/config"
+require("dotenv").config()
+const mongoose =  require("mongoose")
 const express = require("express")
 const app = express()
+const path = require("path")
+app.use(express.json())
 
 const userRoute = require("./server/routes/user")
+
+mongoose.connect(process.env.dbURL) 
+    .then(() => console.log("DB Connected!!"))
+    .catch(error => console.log(error));
 
 //CORS middleware
 app.use(function(req, res, next) {
@@ -10,6 +19,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     next();
 });
+
+app.use(express.static(__dirname + "/public"))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public", "index.html")))
 
 app.use("/users", userRoute)
 
